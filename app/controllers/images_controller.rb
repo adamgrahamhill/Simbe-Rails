6c76c4ba792e2af62454f40e6ac9a79f2d8d3391
Adam Hill <enjoilif3@gmail.com>
2016-07-30 16:06:49 -0400

    Setup image attachments

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,29 @@
 class ImagesController < ApplicationController
+	before_action :set_image, only: [:show, :update, :edit, :destroy]
+	
+	def index
+		@images = Image.all
+	end
+
+	def new
+		@image = Image.new
+	end
+
+	def create
+		@image = Image.new(image_params)
+		
+		if @image.save
+			redirect_to @image, notice: "Image saved"
+		else
+			render :new
+		end
+	end
+
+	private
+	def image_params
+		params.require(:image).permit(:name, :screen_cap)
+	end
+	def set_image
+		@image = Image.find(params[:id])
+	end
 end
