c3b6f5a050da9e7dad17e6d491506ec503320ded
Adam Hill <enjoilif3@gmail.com>
2016-07-31 12:21:21 -0400

    Change password attribute to password_digest for bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
-	validates :name, :email, :password, presence: true
+	has_secure_password
+	validates :name, 
+	presence: true
+	validates :email, 
+				presence: true,
+				uniqueness: true,
+				format: {
+					with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+				}
+	def to_s
+		"#{name}"
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731000626) do
+ActiveRecord::Schema.define(version: 20160731162002) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string   "title"
@@ -45,10 +45,10 @@ ActiveRecord::Schema.define(version: 20160731000626) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
     t.boolean  "admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
